c5453a8588f5b2b2af7da79c7ce60fb4f754c8c0
Samuel Sempere <chempe@gmail.com>
2023-05-11 23:22:50 +0200

    asdasd

---
+++ app/server.js
@@ -0,0 +1,25 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+const strapi = require('./backend');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const server = createServer(async (req, res) => {
+  const parsedUrl = parse(req.url, true);
+  const { pathname, query } = parsedUrl;
+
+  if (pathname.startsWith('/api')) {
+    await strapi.middleware(req, res, () => {});
+  } else {
+    handle(req, res, parsedUrl);
+  }
+});
+
+app.prepare().then(() => {
+  server.listen(process.env.PORT || 3000, () => {
+    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`);
+  });
+});
